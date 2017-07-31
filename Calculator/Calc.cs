f33b2da5a85a87a96eec9771019e290e80b22f54
Oyakodon <oykdn@oykdn.com>
2017-07-31 15:34:13 +0900

    足し算と引き算を実装

---
+++ Calculator/Calc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calc
+    {
+        // 足し算
+        public int Sum(int x, int y)
+        {
+            int z;
+            z = x - y;
+            return z;
+        }
+
+        // 引き算
+        public int Sub(int x, int y)
+        {
+            int z;
+            z = x - y;
+            return z;
+        }
+
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,6 +10,38 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            int x, y;
+
+            // 数字の入力
+            Console.Write("x >");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("y >");
+            y = int.Parse(Console.ReadLine());
+
+            // 計算するよ
+            Console.Write("+/- >");
+            var input = Console.ReadLine();
+            var op = input[0];
+
+            var calc = new Calc();
+            int z;
+            switch(op)
+            {
+                case '+':
+                    z = calc.Sum(x, y);
+                    Console.WriteLine("Ans : " + z);
+                    break;
+
+                case '-':
+                    z = calc.Sum(x, y);
+                    Console.WriteLine("Ans : " + z);
+                    break;
+
+                default:
+                    Console.WriteLine("しらん : " + op);
+                    break;
+            }
+
         }
     }
 }
